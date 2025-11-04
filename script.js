c1620956a0ae370d1a17cc9228c068ea17613cbd
myKalling <owner@mykalling.net>
2025-11-04 10:56:47 -0800

    Contact Form Work

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function toggleMenu(evt) {
         nav.style.opacity = 1;
     }
 }
+
+DocumentTimeline.getElementById("contactForm").addEventListener("Submit", function(event)) {
+    // prevent deafault form submission behavior
+    event.preventDefault();
+
+    //collect form data
+    const firstName = document.querySelector("fname");
+    const lastName = document.querySelector("lname");
+    const email = document.querySelector("email");
+    const message = document.querySelector("message");
+
+    // log form data to console (for demonstration purposes)
+    console.log(`Form Submission: $(firstName) $(lastName), $(email), $(message)`);
+
+    // reset the form
+    document.getElementById("contactForm").reset();
+});
